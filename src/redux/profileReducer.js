5e63810ae6a98f393e262971490f87749c6c7db9
Hakob <hakob.andreasyan@popupschool.com>
2021-07-07 18:05:32 +0400

    test adedd , next lesson > 91(90) redyx-ducks

--- src/redux/profileReducer.js
+++ src/redux/profileReducer.js
@@ -3,6 +3,7 @@ import {profileAPI, usersAPI} from "../api/api";
 const ADD_POST = "ADD-POST";
 const SET_USER_PROFILE = "SET_USER_PROFILE";
 const SET_STATUS = "SET_STATUS";
+const DELETE_POST = "DELETE_POST";
 
 let initialState = {
   postsData: [
@@ -57,6 +58,11 @@ const profileReducer = (state = initialState, action) => {
         ...state,
         status: action.status,
       };
+    case DELETE_POST:
+      return {
+        ...state,
+        postsData:state.postsData.filter(post=>post.id !== action.postId)
+      }
     default:
       return state;
   }
@@ -65,6 +71,7 @@ export default profileReducer;
 
 export const addPostActionCreator = (newPostText) => ({ type: ADD_POST,newPostText });
 export const setStatus = (status) => ({ type: SET_STATUS ,status});
+export const deletePost = (postId) => ({ type: DELETE_POST ,postId});
 export const setUserProfile = (profile) => ({
   type: SET_USER_PROFILE,
   profile,
